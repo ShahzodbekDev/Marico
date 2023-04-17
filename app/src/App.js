1c3b810e59dbfe59c3a27c3c91f7e6026b225dd7
ShahzodbekDev <shahzodbekbloger777@gmail.com>
2023-04-17 10:18:56 +0500

    Update Home page

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "./Pages/header";
+import Banner from "./Pages/banner";
 
 function App() {
   return <div className="App ">
    <Header/>
+   <Banner/>
    
   </div>;
 }
--- app/src/Pages/banner.jsx
+++ app/src/Pages/banner.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import Text from "../Images/Text_shape.png";
+import Dot from "../Images/Dot.png";
 
 const Banner = () => {
   return (
-    <div className='banner limit_width'></div>
-  )
-}
+    <div className="banner limit_width">
+      <h1>Own your audience.</h1>
+      <h1 className="gradien_trxt">So you don't lose them.</h1>
 
-export default Banner
+      <h4>Turn your audience into email and </h4>
+      <h4>text message subscribers.</h4>
+      <div className="but_bloc">
+        <button className="one_but">Get Started Now</button>
+        <button className="tou_but">View A Demo</button>
+      </div>
+      <p>
+        <span>
+          <img src={Dot} />
+          1000+
+        </span>&nbsp;
+        creators have already started
+      </p>
+    </div>
+  );
+};
+
+export default Banner;
